04720924a7aef20e6b67b726256352eeb6bb8ee4
Lucas Silva <lucasluizss@live.com>
2025-09-12 11:55:27 +0100

    fix: open dialog from center

--- libs/zard/src/lib/components/dialog/dialog.variants.ts
+++ libs/zard/src/lib/components/dialog/dialog.variants.ts
@@ -1,6 +1,6 @@
 import { cva, VariantProps } from 'class-variance-authority';
 
 export const dialogVariants = cva(
-  'fixed left-[50%] top-[50%] z-50 grid w-full translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg rounded-lg max-w-[calc(100%-2rem)] sm:max-w-[425px] duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out',
+  'fixed left-[50%] top-[50%] z-50 grid w-full translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg rounded-lg max-w-[calc(100%-2rem)] sm:max-w-[425px] duration-100 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=open]:fade-in-0 data-[state=closed]:fade-out-0 data-[state=open]:zoom-in-95 data-[state=closed]:zoom-out-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] data-[state=closed]:duration-300 data-[state=open]:duration-300',
 );
 export type ZardDialogVariants = VariantProps<typeof dialogVariants>;
